d5e859e4ed8ad3362e23ea96d1e2bae5cdda1d98
dungnv <dungnv@nal.vn>
2014-12-18 15:04:11 +0700

    Update flickr seach method, key ...

--- popup.js
+++ popup.js
@@ -8,7 +8,9 @@
  *
  * @type {string}
  */
-var QUERY = 'nam_ji_hyun';
+// var QUERY = 'nam_ji_hyun';
+// Tuy theo key_word muon search
+var QUERY = 'suzy';
 
 var kittenGenerator = {
   /**
@@ -20,6 +22,7 @@ var kittenGenerator = {
    * @type {string}
    * @private
    */
+   /*
   searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
       'method=flickr.photos.search&' +
       'api_key=8441b9e02088b4adf5da163c840c6554&' +
@@ -28,6 +31,22 @@ var kittenGenerator = {
       'content_type=1&' +
       'sort=interestingness-desc&' +
       'per_page=32',
+      */
+
+  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
+      'method=flickr.photos.search&' +
+      'api_key=8441b9e02088b4adf5da163c840c6554&' +
+      'text=' + encodeURIComponent(QUERY) + '&' +
+      'safe_search=0&' +
+      'content_type=1&' +
+      'sort=interestingness-desc&' +
+      'per_page=64',
+  // searchOnFlickr_2: 'https://api.flickr.com/services/rest/?method=flickr.favorites.getList&api_key=425c152e185662ffdf62ee7ef081c931&user_id=58013055%40N05&per_page=30&format=rest&auth_token=72157649406355470-72e5b8af0aacc09e&api_sig=f70d190bba8b02b95c623fb3ba34f444',
+  searchOnFlickr_3: 'https://api.flickr.com/services/rest/?method=flickr.favorites.getList&api_key=425c152e185662ffdf62ee7ef081c931&user_id=58013055%40N05&per_page=124&format=rest&auth_token=72157649406355470-72e5b8af0aacc09e&api_sig=9b0c96421000f8fddd1e533ca3dc7b18',
+  searchOnFlickr_3k: 'https://api.flickr.com/services/rest/?method=flickr.favorites.getList&api_key=0c9ed249fe9b21cad9ff2671e55ed5f9&user_id=58013055%40N05&per_page=3000&format=rest&auth_token=72157649821504785-002ec82f606a7f76&api_sig=54f7dbc3594fd6663d837df2986bdeba',
+
+  // api_sig no hash ca per_page vao nen ko doi duoc per_page NEU ko co API_SIG 
+  // ca token cung khac, --> lieu copy sang chrome PC khac con run ko ?
 
   /**
    * Sends an XHR GET request to grab photos of lots and lots of kittens. The
@@ -37,7 +56,7 @@ var kittenGenerator = {
    */
   requestKittens: function() {
     var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
+    req.open("GET", this.searchOnFlickr_3k, true);
     req.onload = this.showPhotos_.bind(this);
     req.send(null);
   },
